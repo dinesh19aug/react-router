8b3bdf8a8b1a9addd2b3dea5f3c30b4ef5f16c23
dinesh19aug <dinesh19aug@gmail.com>
2023-05-07 17:40:20 -0400

    EsLint clean up

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ function About (){
 
         return (
             <div className="about-page-container">
-                <img alt="hero Image" src={bgImg} className="about-hero-image" />
+                <img alt="hero" src={bgImg} className="about-hero-image" />
                 <div className="about-page-content">
                     <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
                     <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
